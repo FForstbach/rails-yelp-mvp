c7232108601b30fe03dfb0dcc59fe0eeb2c1b068
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-04-19 11:49:43 +0200

    added validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do |whatever|
+      Restaurant.create(name: Faker::Superhero.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone, category: "chinese")
+      Restaurant.create(name: Faker::Superhero.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone, category: "italian")
+      Restaurant.create(name: Faker::Superhero.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone, category: "japanese")
+      Restaurant.create(name: Faker::Superhero.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone, category: "french")
+      Restaurant.create(name: Faker::Superhero.name, address: Faker::Address.street_name, phone_number: Faker::PhoneNumber.cell_phone, category: "belgian")
+end
